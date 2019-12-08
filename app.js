f7fb88dccca700a6f36a4c0e5d5fc181f94bff4e
leolikestocode <leonardofpereira25@gmail.com>
2019-12-08 20:16:50 -0300

    Finished exercise 3

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const path = require('path')
 
 const app = express()
 
+const rootDir = require('./util/path')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(rootDir, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'notfound-404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', 'notfound-404.html'))
 })
 
 app.listen(3000)
