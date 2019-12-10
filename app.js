561701ff332a1278d163d1ec534313ba53c685ec
leolikestocode <leonardofpereira25@gmail.com>
2019-12-09 22:42:21 -0300

    Finished exe4

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+const expressHbs = require('express-handlebars')
 const app = express()
 
+app.engine('hbs', expressHbs())
+// app.set('view engine', 'hbs')
+
+// pug engine
+app.set('view engine', 'pug')
+
+app.set('views', 'views')
+
 const rootDir = require('./util/path')
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(rootDir, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', 'notfound-404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 app.listen(3000)
